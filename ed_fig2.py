39875cc9de024554364e6aa962295779a6c3ef0f
Simon Lee <51027902+simonleewx@users.noreply.github.com>
2019-06-04 23:40:28 -0500

    Update ed_fig2.py

--- ed_fig2.py
+++ ed_fig2.py
@@ -32,7 +32,7 @@ for count,year in enumerate(years):
 	latidx1 = np.where(lats==70)[0][0]
 	latidx2 = np.where(lats==30)[0][0]
 	lonidx1 = np.where(lons==280)[0][0]
-	lonidx2 = np.where(lons==357.5)[0][0]
+	lonidx2 = np.where(lons==350)[0][0]
 
 	latbox = lats[latidx1:latidx2+1]
 
